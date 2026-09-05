b2e60ed353a93069a91cad3dbb8963ed780a9fcc
uzair <uzairsiddiqui0707@gmail.com>
2026-09-05 21:50:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const form = document.querySelector("#github-form");
-const usernameInput = document.querySelector("#github-username");
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const username = usernameInput.value;
-
-    console.log("Searching for:", username);
-});
